e22adc3c36f50394a2d2ab8db6a69430b33e845a
JMASA22 <janamasayos@gmail.com>
2022-12-19 14:02:30 +0100

    bolitas

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ pg.display.set_caption("Boles rebotant") #títol finestra
 
 #variable per controlar el bucle
 game_over = False
+
+x = 400
+y = 300
+vx = 1
+vy = 1
+
 while not game_over:
 
     for events in pg.event.get(): #captura tots els objectes/events en forma de llista
@@ -18,8 +24,16 @@ while not game_over:
         if events.type == pg.QUIT: #per parar bulce, per ubicar l'element final, sinó no pararia de tenir esdevenimets
             game_over = True
 
-pantalla_principal.fill ((52, 152, 220)) #assignar color pantalla
-pg.draw.rect(pantalla_principal, (200, 60, 40),(400,300,40,40)) #colors rectangle + posició+tamany
-pg.display.flip()
+    pantalla_principal.fill ((52, 152, 220)) #assignar color pantalla
+    y += vy
+    x += vx
 
+    if x >= 800 or x == 0: #arribi al límit de la pantalla
+        vx *= -1
+    if y >= 600 or y == 0:
+        vy *= -1
+
+    pg.draw.rect(pantalla_principal, (200, 60, 40),(,580,20,20)) #colors rectangle + posició+tamany
+    pg.display.flip()
+    
 pg.quit()
